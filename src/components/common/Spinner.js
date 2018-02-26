24be4181552d88d6c4dcbb63fe0499e06f21da77
Erik Sand <clawtang@gmail.com>
2018-02-25 16:05:12 -0800

    Init repo

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  View,
+  ActivityIndicator,
+} from 'react-native';
+
+const Spinner = ({ size }) => {
+  const { containerStyle } = styles;
+
+  return (
+    <View style={containerStyle}>
+      <ActivityIndicator
+        size={size || 'large'}
+      />
+    </View>
+  );
+};
+
+export { Spinner };
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// this file seems to map out the state object for the app
+import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
+
+export default combineReducers({
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
+});
